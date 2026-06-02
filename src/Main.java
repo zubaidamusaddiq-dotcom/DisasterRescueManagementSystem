e39b119e3e3e5603c847f6657abffeb22df93948
LENOVO <zubaidamusaddiq@gmail.com>
2026-06-02 16:24:28 +0500

    Updated Java code

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,44 @@ class disaster{
                 ", City: " + city +
                 ", Severity: " + severity;
     }
-
-
+}
+class treenode {
+    disaster disaster;
+    treenode left, right;
+    public treenode(disaster disaster) {
+        this.disaster = disaster;
+        left = right = null;
+    }
+}
+class Disasterbst {
+    treenode root;
+    public void insert(disaster disaster) {
+        root = insertRec(root, disaster);
+    }
+    private treenode insertRec(treenode root, disaster disaster) {
+        if (root == null) {
+            return new treenode(disaster);
+        }
+        if (disaster.id < root.disaster.id) {
+            root.left = insertRec(root.left, disaster);
+        } else {
+            root.right = insertRec(root.right, disaster);
+        }
+        return root;
+    }
+    public disaster search(int id) {
+        return searchRec(root, id);
+    }
+    private disaster searchRec(treenode root, int id) {
+        if (root == null) {
+            return null;
+        }
+        if (root.disaster.id == id) {
+            return root.disaster;
+        }
+        if (id < root.disaster.id) {
+            return searchRec(root.left, id);
+        }
+        return searchRec(root.right, id);
+    }
 }
